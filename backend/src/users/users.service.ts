888fa03a9336fce709e01090f941533753bd83b0
Danilo Denic <danilo.denic@elfak.rs>
2026-07-21 14:18:59 +0200

    feat: complete UserService implementation

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "./user.entity";
 import { Repository } from "typeorm";
+import { CreateUserDto } from "./dto/create-user.dto";
 
 @Injectable()
 export class UsersService {
@@ -17,4 +18,14 @@ export class UsersService {
     async findByEmail(email: string): Promise<User | null> {
         return this.userRepository.findOne({ where: { email } });
     }
+
+    async create(createUserDto: CreateUserDto): Promise<User> {
+        const user = this.userRepository.create(createUserDto);
+        return this.userRepository.save(user);
+    }
+
+    async findById(id: number): Promise<User | null> {
+        return this.userRepository.findOne({ where: { id } });
+    }
+
 }
