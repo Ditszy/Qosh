10faff75788b456759afadc076eaa0343a59fa5d
Danilo Denic <danilo.denic@elfak.rs>
2026-07-22 12:51:24 +0200

    fix: usernameField in constructor of LocalStrategy

--- backend/src/auth/local.strategy.ts
+++ backend/src/auth/local.strategy.ts
@@ -1,12 +1,12 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-jwt";
+import { Strategy } from "passport-local";
 import { AuthService } from "./auth.service";
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
     constructor(private authService: AuthService) {
-        super({ usernameField: 'email' }); //to fix
+        super({ usernameField: 'email' });
     }
 
     async validate(email: string, password: string): Promise<any> {
