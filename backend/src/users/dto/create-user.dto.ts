98a45234e8408c95aca7e84b4696c58aad1b2d65
Danilo Denic <danilo.denic@elfak.rs>
2026-07-22 13:40:46 +0200

    fix: add ApiProperty decorator to LoginDto and CreateUserDto

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,18 +1,23 @@
 import { IsEmail, IsEnum, IsNotEmpty, IsString, MinLength } from 'class-validator';
 import { UserRole } from '../../common/user-role.enum';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     name!: string;
 
+    @ApiProperty()
     @IsEmail()
     email!: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     @MinLength(6)
     password!: string;
 
+    @ApiProperty()
     @IsEnum(UserRole)
     role!: UserRole;
 }
