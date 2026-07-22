922c0ab93400816390b46649198b1da421e58ae4
Danilo Denic <danilo.denic@elfak.rs>
2026-07-22 13:37:27 +0200

    fix: changes in AppModule and implemented AuthModule

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,28 @@
+import { Module } from "@nestjs/common";
+import { UsersModule } from "../users/users.module";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { AuthController } from "./auth.controller";
+import { LocalStrategy } from "./local.strategy";
+import { JwtStrategy } from "./jwt.strategy";
+import { AuthService } from "./auth.service";
+
+@Module({
+    imports: [
+        UsersModule,
+        PassportModule,
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+                secret: config.get('JWT_SECRET'),
+                signOptions: { expiresIn: config.get('JWT_EXPIRATION', '3600') },
+            }),
+        }),
+    ],
+    controllers: [AuthController],
+    providers: [AuthService, LocalStrategy, JwtStrategy],
+    exports: [AuthService]
+})
+export class AuthModule { }
