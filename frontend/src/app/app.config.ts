31d404d07b45b0232605d766db6b69257b8da90d
Danilo Denic <danilo.denic@elfak.rs>
2026-07-31 14:20:07 +0200

    Register auth store reducer

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,13 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { provideStore } from '@ngrx/store';
 
 import { routes } from './app.routes';
+import { authFeatureKey, authReducer } from './core/auth/auth.state';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideStore({ [authFeatureKey]: authReducer }),
+  ],
 };
