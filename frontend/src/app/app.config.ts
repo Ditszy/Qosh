5f4036af3e8c9b1dcc91486950066375e5441ad3
Danilo Denic <danilo.denic@elfak.rs>
2026-08-01 11:44:37 +0200

    Register auth token interceptor

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,16 +1,17 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { provideStore } from '@ngrx/store';
 
 import { routes } from './app.routes';
 import { provideApiBaseUrl } from './core/api';
+import { authTokenInterceptor } from './core/auth/auth-token.interceptor';
 import { authFeatureKey, authReducer } from './core/auth/auth.state';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authTokenInterceptor])),
     provideApiBaseUrl(),
     provideRouter(routes),
     provideStore({ [authFeatureKey]: authReducer }),
