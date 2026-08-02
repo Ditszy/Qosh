64895df649b5315842c2412c0c7a512ddb00165f
Danilo Denic <danilo.denic@elfak.rs>
2026-08-02 15:00:16 +0200

    Add index file to export statistics services and models

---
+++ frontend/src/app/features/statistics/index.ts
@@ -0,0 +1,24 @@
+export { PlayerRankingsService } from './player-rankings.service';
+export { StatisticsApiService } from './statistics-api.service';
+export type {
+  LeaderCategory,
+  MatchStatistics,
+  MatchTeamStatistics,
+  NormalizedPlayerStatisticsFilters,
+  PlayerMatchStatistic,
+  PlayerProfile,
+  PlayerRankingsState,
+  PlayerRecentMatchStatistic,
+  PlayerStatistic,
+  PlayerStatisticLeader,
+  PlayerStatisticsFilters,
+  PlayerStatisticSort,
+  PublicUser,
+  SortDirection,
+  StatisticLine,
+  StatisticMatchStatus,
+  StatisticsMatchSummary,
+  StatisticsTeamSummary,
+  StatisticsTournamentSummary,
+  StatisticTotals,
+} from './statistics.models';
