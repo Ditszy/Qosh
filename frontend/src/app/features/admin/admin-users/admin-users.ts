9727d4e64ffdce544620ccf4c09cf8215d734d24
Danilo Denic <danilo.denic@elfak.rs>
2026-08-10 11:47:11 +0200

    Load admin users list

--- frontend/src/app/features/admin/admin-users/admin-users.ts
+++ frontend/src/app/features/admin/admin-users/admin-users.ts
@@ -1,9 +1,17 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { inject } from '@angular/core';
+
+import { AdminUsersApiService } from '../admin-users-api.service';
 
 @Component({
   selector: 'app-admin-users',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './admin-users.html',
   styleUrl: './admin-users.scss',
 })
-export class AdminUsers {}
+export class AdminUsers {
+  private readonly adminUsersApi = inject(AdminUsersApiService);
+
+  readonly users$ = this.adminUsersApi.listUsers();
+}
